198c0e98db5401d519ee3b8118130e144bef7702
AswinKumar <146310707+aswin-kumar-93@users.noreply.github.com>
2023-09-28 14:44:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const a=document.createElement('button');
+a.innerHTML="B";
+const firstButton=document.createElement('button');
+firstButton.innerText='Click Me';
+firstButton.className="butt";
+const secondButton=document.createElement('button');
+secondButton.innerText='Click Me';
+secondButton.className="butt";
+a.style.height="15pc";
+a.style.width="15pc";
+a.style.borderRadius="50%"
+a.style.marginLeft="10%";
+document.body.append(firstButton);
+document.body.append(secondButton);
+document.body.append(a);
+firstButton.onclick=(off)=>{a.style.backgroundColor="#F50808";
+var x = document.createElement("AUDIO");
+
+};
+secondButton.onclick=(on)=>{ a.style.backgroundColor="#00FF00";}
